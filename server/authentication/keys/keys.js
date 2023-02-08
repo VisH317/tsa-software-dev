91d0745881735c24eb57bec5843248bc237cf368
VisH317 <vt201916384@gmail.com>
2023-02-07 20:52:29 -0500

    fixed keys issue

--- server/authentication/keys/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "MongoURI": "mongodb+srv://dbadmin:MEdPS0i2DjgAHJAL@cluster0.f0jqkdf.mongodb.net/?retryWrites=true&w=majorit",
-    "JWTSecret": "qaewiofjkajsdbgklajeshvrqbiwuenhfaldsijfaewiobthlawiehnfcawileufcnhlawih32i745y3287rheaiufhewhhfiadshufakdsj"
-}
