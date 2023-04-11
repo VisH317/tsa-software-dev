0f245f9a36408d421bda7b8354522b9424489115
AC_is_Cool <choudharyaditya974@gmail.com>
2023-04-11 18:17:42 -0400

    ai reponse is shown in ui

--- software_dev_tsa/src/pages/api/chatbot.js
+++ software_dev_tsa/src/pages/api/chatbot.js
@@ -36,7 +36,7 @@ export default async function generatePrompt (req, res) {
             temperature: 0.6,
         });
         console.log("completion", completion, "completion.data", completion.data,"completion.data.choices", completion.data.choices )
-        res.status(200).json({ result: completion.data.choices[0].text });
+        res.status(200).json({  result: completion.data.choices[0].text });
     } catch (error) {
         // Consider adjusting the error handling logic for your use case
         if (error.response) {
