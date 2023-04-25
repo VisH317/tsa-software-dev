d7531b8f5f0706c4f0eacb72a5ef842049393eb8
Vis <vt201916384@gmail.com>
2023-04-24 20:15:03 -0400

    started student lecture UI

--- software_dev_tsa/src/pages/student/lecture/[id].tsx
+++ software_dev_tsa/src/pages/student/lecture/[id].tsx
@@ -7,6 +7,8 @@ import { Lecture } from '@/lib/classData'
 import { useUser, Email } from '@/lib/user'
 import { TextField } from '@mui/material'
 import Modal from '@/components/Modal/Modal'
+import MiniDrawer from '@/components/Dashboard/Drawer'
+import { montserrat } from '@/styles/fonts'
 
 // IMPORTANT FOR LATER: add check for lecture being accessed is in the right class
 
@@ -130,37 +132,65 @@ export default function StudentLecture() {
 
     const [note, setNote] = useState<string>("")
 
+    // modal states
+    const [drawer, setDrawer] = useState<boolean>(false)
+
     if(status==="loading") return <div>LOADING</div>
     if(status==='error') return <div>error</div>
+
+    const [seeResponsesModal, setSeeResponsesModal] = useState<boolean>(false)
     
     return (
-        <>
-            <p>{students}</p>
-            <button onClick={closeRoom}>Close Room</button>
-            <br/><br/><br/>
-            <form onSubmit={submitQuestion}>
-                <p>HAVE A QUESTION?</p>
-                <textarea value={question} placeholder="Question:" rows={3} cols={25} onChange={(e) => setQuestion(e.target.value)}/>
-                <button type="submit">Ask Question</button>
-            </form>
-            <br/>
-            <div>
-                Questions from the teacher:
-                {mapTeacherQuestions()}
-            </div>
-            <br/><br/>
-            <div>
-                Notes to take:
-                <textarea value={note} onChange={e => setNote(e.target.value)} rows={20} cols={50}/>
-            </div>
-            <p>Answer to your last question: {answer}</p>
-            <Modal open={open} close={() => setOpen(false)}>
+        <div className={`${montserrat.variable} font-sans`}>
+            <MiniDrawer open={drawer} handleDrawerClose={() => setDrawer(false)} handleDrawerOpen={() => setDrawer(true)}>
+            <div className="w-full h-screen relative flex flex-col">
+                    <div className="grow flex flex-row">
+                        <div className="w-[60%] h-full flex flex-col">
+                            <div className="h-[60%]">
+                                <div>
+                                    Notes to take:
+                                    <textarea value={note} onChange={e => setNote(e.target.value)} rows={20} cols={50}/>
+                                </div>
+                            </div>
+                            <div className="h-[40%] p-10 bg-slate-100">
+                                <p className="text-4xl font-medium text-slate-700">Have a Question?</p>  
+                                <p>Answer to your last question: {answer}</p>
+                                <form onSubmit={submitQuestion}>
+                                    <textarea value={question} placeholder="Question:" rows={3} cols={25} onChange={(e) => setQuestion(e.target.value)}/>
+                                    <button type="submit">Ask Question</button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div className="w-[40%] h-full bg-slate-200 p-10">
+                            <div>
+                                <p className="text-5xl font-medium">Teacher Questions</p>
+                                <div className="h-16"/>
+                                <div className="w-full h-full flex flex-col">
+                                    {mapTeacherQuestions()}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-none bg-slate-700 w-full h-32 flex flex-row p-10 align-center">
+                        <div className="w-1/2 flex justify-start align-center">
+                            <p className="text-4xl font-medium text-slate-200">Students in Session: {students}</p>
+                        </div>
+                        <div className="w-1/2 flex justify-end align-center pr-[100px] gap-10">
+                            {/* <button className='bg-green-500 px-3 py-2 text-white font-medium rounded-lg border-slate-100 hover:-translate-y-1 hover:border-green-200 hover:bg-green-600 duration-300' onClick={closeRoom}>Ask Question</button> */}
+                            <button className="bg-red-600 px-3 py-2 text-white font-medium rounded-lg border-slate-700 hover:-translate-y-1 hover:bg-red-700 duration-300" onClick={leave}>Leave Lecture</button>
+                        </div>
+                    </div>
+                </div>
+            </MiniDrawer>
+            <Modal open={open} close={() => setOpen(false)} height="50vh">
                 <h1>Submit answer to question</h1>
                 <p>Question: {currentTeacherQuestion?.prompt}</p>
                 <textarea rows={5} cols={30} value={studentAnswer} onChange={e => setStudentAnswer(e.target.value)}/>
                 <button onClick={submitAnswer}>Submit Answer</button>
             </Modal>
-        </>
+
+        </div>
     )
     // NOTES: events to emit - createRoom, deleteRoom, for student: joinRoom leaveRoom (with notes UI)
     // render a list of people in the meeting and change when receiving a leave or join room
